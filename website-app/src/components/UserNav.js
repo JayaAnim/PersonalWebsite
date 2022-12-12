999329daf65f33ebfa0e464eaa76799b15fab784
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2022-12-11 23:13:56 -0600

    Phase 2 of navbar

--- website-app/src/components/UserNav.js
+++ website-app/src/components/UserNav.js
@@ -6,30 +6,43 @@ import './UserNav.css'
 
 export default function UserNav() {
   const navRef = useRef();
-  const[isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [homeSelected, setHomeSelected] = useState(false);
 
   const showNavbar = () => {
+    setIsOpen(!isOpen);
     navRef.current.classList.toggle('responsive_nav');
   }
+
   return (
     <header>
       <h3>Logo</h3>
       <nav ref={navRef}>
-        <div className='col d-flex align-items-center h-100'>
+        {isOpen ? <>
           <Link to='/' className='navLink row'>Home</Link>
-        </div>
-        <div className='col d-flex align-items-center h-100'>
           <Link to='AboutPage' className='navLink'>About</Link>
-        </div>
-        <div className='col d-flex align-items-center h-100'>
           <Link to='ProjectPage' className='navLink'>Projects</Link>
-        </div>
-        <div className='col d-flex align-items-center h-100'>
           <Link to='ResumePage' className='navLink'>Resume</Link>
-        </div>
-        <div className='col d-flex align-items-center h-100'>
           <Link to='ContactPage' className='navLink'>Contact</Link>
-        </div>
+        </> :
+          <>
+            <div className='col d-flex align-items-center h-100'>
+              <Link to='/' className='navLink'>Home</Link>
+            </div>
+            <div className='col d-flex align-items-center h-100'>
+              <Link to='AboutPage' className='navLink'>About</Link>
+            </div>
+            <div className='col d-flex align-items-center h-100'>
+              <Link to='ProjectPage' className='navLink'>Projects</Link>
+            </div>
+            <div className='col d-flex align-items-center h-100'>
+              <Link to='ResumePage' className='navLink'>Resume</Link>
+            </div>
+            <div className='col d-flex align-items-center h-100'>
+              <Link to='ContactPage' className='navLink'>Contact</Link>
+            </div>
+          </>
+        }
         <button className='nav-btn nav-close-btn' onClick={showNavbar}>
           <FaTimes/>
         </button>
