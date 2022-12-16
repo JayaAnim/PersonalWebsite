35d0d021044bc0644f6f2071cdb8774b623ea43d
Chase Lamkin <chaselamkin@Chases-MacBook-Pro.local>
2022-12-16 17:51:59 -0600

    Navbar complete, homepage complete, about page started

--- website-app/src/components/AboutPage.js
+++ website-app/src/components/AboutPage.js
@@ -1,8 +1,41 @@
+import './styles/AboutPage.css';
+
 export default function AboutPage() {
     return (
         <>
-            <div>
-                About Page
+            <div className='container-fluid aboutPage'>
+                <div className='col-12 d-flex flex-wrap align-items-center h-100'>
+                    <div className='col-6 col-lg-4 d-flex h-25'>
+                        <div className='card row d-flex w-100 m-2'>
+
+                        </div>
+                    </div>
+                    <div className='col-6 col-lg-4 d-flex h-25'>
+                        <div className='card row d-flex w-100 m-2'>
+
+                        </div>
+                    </div>
+                    <div className='col-6 col-lg-4 d-flex h-25'>
+                        <div className='card row d-flex w-100 m-2'>
+
+                        </div>
+                    </div>
+                    <div className='col-6 col-lg-4 d-flex h-25'>
+                        <div className='card row d-flex w-100 m-2'>
+
+                        </div>
+                    </div>
+                    <div className='col-6 col-lg-4 d-flex h-25'>
+                        <div className='card row d-flex w-100 m-2'>
+
+                        </div>
+                    </div>
+                    <div className='col-6 col-lg-4 d-flex h-25'>
+                        <div className='card row d-flex w-100 m-2'>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
