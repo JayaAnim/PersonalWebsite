57a54fb1721dd0188accd85449da7e9bb5ccfbc9
Chase Lamkin <chaselamkin@Chases-MacBook-Pro.local>
2022-12-18 22:06:06 -0600

    Big progress on card expansion

--- website-app/src/components/AboutCard.js
+++ website-app/src/components/AboutCard.js
@@ -2,19 +2,42 @@ import '../styles/AboutCard.css';
 import { useEffect, useRef, useState} from 'react';
 
 export default function Card(props) {
-    const [isExpanded, setIsExpanded] = useState(false);
+    //Props to display initial card values
     const textContent = props.textContent;
     const delay = props.delay;
     const zoom = props.zoom;
 
+    //States and refs to handle expansion
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [style, setStyle] = useState({});
+    const containerRef = useRef();
+
+    useEffect(() => {
+        if (isExpanded) {
+            setStyle({
+                transform: `translate(-${containerRef.current.offsetLeft - props.parentLeft}px, -${containerRef.current.offsetTop -65}px)`,
+                height: `${props.parentHeight * .9}px`,
+                zIndex: '5',
+                transition: '1.5s'
+            });
+        } else {
+            setStyle({
+                transform: `translate(0px, 0px)`,
+                transition: '1.5s'
+            });
+        }
+    }, [isExpanded]);
+
+    // height: '75vh',
     const handleClick = () => {
         setIsExpanded(!isExpanded);
-        console.log(isExpanded);
+        //containerRef.style.transform = `translate(${containerRef.current.offsetTop}px, ${containerRef.current.offsetLeft}px)`;
     }
+
     return (
         <>
-            <div className='card-container'>
-                <div className='about-card' onClick={handleClick}>
+            <div ref={containerRef} className='card-container bg-danger'>
+                <div className='about-card' onClick={handleClick} style={style}>
                     <div className='card-front'>
                         <span className='text-center'>{textContent}</span>
                     </div>
@@ -25,6 +48,7 @@ export default function Card(props) {
 }
 
     /*
+    containerRef.current.style
     // Calls jiggle function every 3.75 seconds
     useEffect(() => {
         const intervalId = setInterval(() => {
