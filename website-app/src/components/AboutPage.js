a2260eab507f84e6932753524bbe3f4678691d4a
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2022-12-19 14:47:53 -0600

    Big progress

--- website-app/src/components/AboutPage.js
+++ website-app/src/components/AboutPage.js
@@ -1,20 +1,60 @@
 import '../styles/AboutPage.css';
-import portrait from '../img/largeaboutpage.png';
 import { useEffect, useRef, useState} from 'react';
-import Aos from 'aos';
-import AboutCardContainer from './AboutCardContainer';
 
 export default function AboutPage() {
+    const cardContainerRef = useRef();
 
     return (
         <>
-            <div className='container-fluid aboutPage'>
-                {/*<img src={portrait} alt='portrait' className='about-image'/>*/}
+            <div className='container-fluid about-page-container'>
                 <div className='about-image'></div>
-                <div className='row d-flex align-items-center justify-content-end h-100'>
-                    <AboutCardContainer/>
+                <div className='content-container'>
+                    <div ref={cardContainerRef} className='card-container'>
+                        <Card parentRef={cardContainerRef}/>
+                        <Card parentRef={cardContainerRef}/>
+                        <Card parentRef={cardContainerRef}/>
+                        <Card parentRef={cardContainerRef}/>
+                        <Card parentRef={cardContainerRef}/>
+                        <Card parentRef={cardContainerRef}/>
+                    </div>
                 </div>
             </div>
         </>
+    );
+}
+
+function Card(props) {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [style, setStyle] = useState({});
+    const parentRef = props.parentRef;
+    const cardRef = useRef();
+
+    useEffect(() => {
+        if (isExpanded) {
+            setStyle({
+                transform: `translate(${-1 * (cardRef.current.offsetLeft - parentRef.current.offsetLeft)}px, ${-1 * (cardRef.current.offsetTop - parentRef.current.offsetTop)}px)`,
+                height: `${parentRef.current.offsetHeight}px`,
+                zIndex: 5,
+                transition: '1s'
+            });
+        } else {
+            setStyle({
+                transform: 'translate(0, 0)',
+                transition: '1s'
+            });
+        }
+    }, [isExpanded]);
+
+    //                height: `${parentRef.current.offsetHeight}px`,
+
+    const handleClick = () => {
+        setIsExpanded(!isExpanded);
+    }
+    return (
+        <div ref={cardRef} className='about-card' onClick={handleClick} style={style}>
+            <div className='card-front'>
+                <p>hi</p>
+            </div>
+        </div>
     );
 }
