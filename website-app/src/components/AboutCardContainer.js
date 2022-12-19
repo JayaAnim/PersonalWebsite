1c573421df8d1b7debb6a9d0fd7da58071ce4eeb
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2022-12-18 22:06:06 -0600

    Big progress on card expansion

--- website-app/src/components/AboutCardContainer.js
+++ website-app/src/components/AboutCardContainer.js
@@ -1,13 +1,22 @@
 import '../styles/AboutCardContainer.css';
 import { cardNames } from '../data/CardData.js';
 import AboutCard from './AboutCard';
+import { useEffect, useState, useRef } from 'react';
 
 export default function AboutCardContainer() {
+    const pageColRef = useRef();
+    const [pixelsToLeft, setPixelsToLeft] = useState(0);
+    const [pageColHeight, setPageColHeight] = useState(0);
+
+    useEffect(() => {
+        setPixelsToLeft(pageColRef.current.offsetLeft);
+        setPageColHeight(pageColRef.current.offsetHeight);
+    }, []);
     return (
-        <div className='page-column'>
+        <div ref={pageColRef} className='page-column bg-primary'>
             {cardNames.map((o, index) => {
                 return (
-                    <AboutCard key={index} textContent={o.name} delay={o.delay} zoom={o.zoom}/>
+                    <AboutCard key={index} textContent={o.name} delay={o.delay} zoom={o.zoom} parentLeft={pixelsToLeft} parentHeight={pageColHeight}/>
                 )
             })}
         </div>
