36970fbf3ea3b4774ced09859a785b3480720c03
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2022-12-24 11:18:32 -0600

    Reformatted all of about me page

--- website-app/src/components/WorkContent.js
+++ website-app/src/components/WorkContent.js
@@ -3,66 +3,58 @@ import '../styles/WorkContent.css';
 export default function WorkContent() {
     return (
         <div className='work-container'>
-            <div className='work-grid'>
-                <div className='work-grid-title'>
-                    <div className='work-grid-title-job'>
-                        National Guard
-                    </div>
-                    <div className='work-grid-title-duration'>
-                        1.5 years - current
-                    </div>
-                    <div className='work-grid-title-specification'>
-                        Mos - 09R
-                    </div>
-                </div>
-                <div className='work-grid-content'>
-                    <ul>
-                        <li>leadership</li>
-                        <li>working as a team</li>
-                        <li>problem solving under stress</li>
-                        <li>ability to receive, analyze, and critique information provided</li>
-                        <li>interpersonal skills</li>
-                    </ul>
-                </div>
-                <div className='work-grid-content'>
-                        <ul>
-                            <li>problem solving (technician)</li>
-                            <li>communication skills (salesman/technician)</li>
-                            <li>time management (technician)</li>
-                            <li>problem solving (salesman)</li>
-                        </ul>
-                </div>
-                <div className='work-grid-title'>
-                    <div className='work-grid-title-job-right'>
-                        Walmart
-                    </div>
-                    <div className='work-grid-title-duration-right'>
-                        2 years
-                    </div>
-                    <div className='work-grid-title-specification-right'>
-                        <span>Electronics salesman<br /></span>
-                        <span>Automotive technician</span>
-                    </div>
-                </div>
-                <div className='work-grid-title'>
-                    <div className='work-grid-title-job'>
-                        Publix
-                    </div>
-                    <div className='work-grid-title-duration'>
-                        5 months
-                    </div>
-                    <div className='work-grid-title-specification'>
-                        Deli clerk
-                    </div>
-                </div>
-                <div className='work-grid-content'>
-                    <ul>
-                        <li>working as a team</li>
-                        <li>time and stress management</li>
-                        <li>communcation skills</li>
-                    </ul>
-                </div>
+            <div className='work-header'>
+                <span className='work-title'>
+                    National Guard
+                </span>
+                <span className='work-title-information'>
+                    MOS-09R
+                </span>
+                <span className='work-title-information'>
+                    1.5years(current)
+                </span>
+            </div>
+            <div className='work-content'>
+                <li>leadership</li>
+                <li>working as a team</li>
+                <li>problem solving under stress</li>
+                <li>ability to receive, analyze, and critique information provided</li>
+                <li>interpersonal skills</li>
+            </div>
+            <div className='work-header'>
+                <span className='work-title'>
+                    Walmart
+                </span>
+                <span className='work-title-information'>
+                    Automotive Tech & Electronics Salesman
+                </span>
+                <span className='work-title-information'>
+                    2years
+                </span>
+            </div>
+            <div className='work-content'>
+                <li>problem solving (technician)</li>
+                <li>communication skills (salesman/technician)</li>
+                <li>time management (technician)</li>
+                <li>problem solving (salesman)</li>
+            </div>
+            <div className='work-header'>
+                <span className='work-title'>
+                    Publix
+                </span>
+                <span className='work-title-information'>
+                    Deli Clerk
+                </span>
+                <span className='work-title-information'>
+                    5months
+                </span>
+            </div>
+            <div className='work-content'>
+                <li>working as a team</li>
+                <li>time and stress management</li>
+                <li>communcation skills</li>
             </div>
         </div>
     );
 }
+
