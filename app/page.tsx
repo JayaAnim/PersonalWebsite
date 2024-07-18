50e41740dbfb7b01de357712314e1988005b5c20
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2024-07-18 04:43:54 -0500

    Added experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import ExperienceSection from './components/ExperienceSection';
 
 const Home: React.FC = () => {
     return (
         <main>
             <div>
                 <Header />
+                <div className="relative h-3 w-screen bg-[#000000]">
+                </div>
                 <Navbar />
+                <ExperienceSection/>
+
+
                 <div className="h-screen w-screen bg-[#000000]">
                     Section
                 </div>
+
                 <div className="h-screen w-screen bg-[#000000]">
                     Section
                 </div>
